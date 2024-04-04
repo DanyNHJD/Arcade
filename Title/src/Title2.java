3f5202f26b37e4d99e05b325efdffbdb05278553
Lukas Swanson <85359339+squidmenhero@users.noreply.github.com>
2024-04-04 13:05:09 -0500

    title

---
+++ Title/src/Title2.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Title2 extends JFrame implements ActionListener {
+
+    JButton button1;
+    JButton button;
+
+
+
+    Title2(){
+
+
+
+
+       //pong
+        button = new JButton();
+        ImageIcon icon = new ImageIcon("Pong26.png");
+        ImageIcon icon1 = new ImageIcon("Wordle.png");
+
+        button.setBounds(300,350,200,100);
+        button.addActionListener(this);
+        button.setFocusable(false);
+        button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(600,600);
+        this.setVisible(true);
+        this.add(button);
+
+
+        //Wordle
+        button1 = new JButton();
+        button1.setBounds(50,350,200,100);
+        button1.addActionListener(this);
+        button1.setFocusable(false);
+        button1.setIcon(icon);
+        button1.setHorizontalTextPosition(JButton.CENTER);
+        button1.setIcon(icon1);
+        this.add(button1);
+
+
+
+
+ }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+     //so the button starts doing things
+
+        if(e.getSource() == button){
+            System.out.println("yes");
+        }
+    }
+}
---
+++ Title/src/Title3.java
@@ -0,0 +1,11 @@
+public class Title3 {
+
+    public static void main(String[] args){
+
+        Title2 frame = new Title2();
+
+
+    }
+
+
+}
